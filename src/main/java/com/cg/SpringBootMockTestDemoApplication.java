0c44478b6b0dc4dfe6ab01854f11ae6ce6181098
PhantommGeeK <devansh.22b1541054@abes.ac.in>
2026-04-02 11:30:20 +0530

    trigger build

--- src/main/java/com/cg/SpringBootMockTestDemoApplication.java
+++ src/main/java/com/cg/SpringBootMockTestDemoApplication.java
@@ -9,6 +9,7 @@ public class SpringBootMockTestDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMockTestDemoApplication.class, args);
 		System.out.println("Spring Boot Mock Test Demo Application Started...");
+		System.out.println("This application demonstrates how to write unit tests using Mockito in a Spring Boot application.");
 	}
 
 }
